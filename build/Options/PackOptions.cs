54d3b80662b53a3f3a6cc69c3fbf7ec8285b1023
Nice3point <nice3point@gmail.com>
2026-09-06 16:47:56 +0300

    [Solution] Move the package project under a source directory

--- build/Options/PackOptions.cs
+++ build/Options/PackOptions.cs
@@ -3,7 +3,7 @@ namespace Build.Options;
 [Serializable]
 public sealed record PackOptions
 {
-    public string ContentDirectory { get; init; } = Path.Combine("Nice3point.Revit.Api", "Content");
+    public string ContentDirectory { get; init; } = Path.Combine("source", "Nice3point.Revit.Api", "Content");
     public string? PinnedDllVersion { get; init; }
     public string? PinnedDllName { get; init; }
 }
